e2b190ec6cf6ba12820bda198176f37b63e8595f
aesthet1c <sanya.lobanov.2006@mail.ru>
2026-01-09 11:39:04 +0500

    All styles are tagged and converted to classes. Create new component DynamicFormField.tsx. Mini code cleaning

--- src/components/DynamicForm/DynamicForm.tsx
+++ src/components/DynamicForm/DynamicForm.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { useState, FC } from 'react';
 import styles from '../DynamicForm/DynamicForm.module.css';
+import DynamicFormField from './DynamicFormField';
 
-interface fieldState {
+export interface fieldState {
   id: number;
   text: string;
 }
 
-const DynamicForm: React.FC = () => {
-  const [fieldList, setfieldList] = React.useState<fieldState[]>([{ id: 1, text: '' }]);
+const DynamicForm: FC = () => {
+  const [fieldList, setfieldList] = useState<fieldState[]>([{ id: 1, text: '' }]);
 
   const handleFieldAdd = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
     setfieldList((prevFieldValue) => {
-      const lastId: number =
+      const prevValue =
         prevFieldValue.length > 0 ? prevFieldValue[prevFieldValue.length - 1]?.id || 0 : 0;
+      const lastId: number = prevValue;
       const nextId: number = lastId + 1;
       return [...prevFieldValue, { id: nextId, text: '' }];
     });
@@ -36,35 +38,18 @@ const DynamicForm: React.FC = () => {
   };
 
   return (
-    <div className={styles.root}>
+    <div className={styles.Dynamic_Form}>
+      <h1>Dynamic-Form task</h1>
       <form>
         <div className={styles.form}>
-          <label>Dynamic-Form</label>
+          <label className={styles.label_title}>Dynamic-Form</label>
           {fieldList.map((field) => (
-            <div key={field.id}>
-              <h2 className={styles.user_index}>Field {field.id}: </h2>
-              <div className={styles.users_block}>
-                <div className={styles.first_input_block}>
-                  <input
-                    name="text"
-                    type="text"
-                    required
-                    value={field.text}
-                    onChange={(event) => handleFieldChange(event, field.id)}
-                    placeholder={`Введите значение в поле ${field.id}`}
-                  />
-                </div>
-                <div className={styles.remove_button_block}>
-                  <button
-                    type="button"
-                    className={styles.button_remove}
-                    onClick={() => RemoveFieldAdd(field.id)}
-                  >
-                    Remove
-                  </button>
-                </div>
-              </div>
-            </div>
+            <DynamicFormField
+              key={field.id}
+              field={field}
+              handleFieldChange={handleFieldChange}
+              RemoveFieldAdd={RemoveFieldAdd}
+            />
           ))}
           {fieldList.length < 5 && (
             <button type="button" className={styles.button_add} onClick={handleFieldAdd}>
@@ -72,10 +57,10 @@ const DynamicForm: React.FC = () => {
             </button>
           )}
           <div className={styles.list_users_block}>
-            <h2>Список пользователей</h2>
+            <h2 className={styles.h2_user_list}>Список пользователей</h2>
             {fieldList.map((field, id) => (
               <ul key={field.id}>
-                <li>
+                <li className={styles.list_users}>
                   Field {field.id}: {field.text}
                 </li>
               </ul>
